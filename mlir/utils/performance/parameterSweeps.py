7f30da1f4e310ac45f7ba62c821085a190d5fcfe
Umang Yadav <umayadav@amd.com>
2025-06-24 23:08:39 +0000

    Fix parameterSweeps

--- mlir/utils/performance/parameterSweeps.py
+++ mlir/utils/performance/parameterSweeps.py
@@ -21,9 +21,7 @@ from dataclasses import dataclass
 from typing import Callable, Iterable, List, Sequence, Optional, Tuple, TypeVar, Union
 
 import perfRunner
-from perfRunner import ConvConfiguration
-from perfRunner import Paths
-from perfRunner import getArch
+from perfRunner import Paths, ConvConfiguration, getArch, getNumCU
 from perfCommonUtils import CORRECT_RESULT_RE
 
 @dataclass(frozen=True)
@@ -527,7 +525,7 @@ def main() -> bool:
             print(f"""Unknown arch {arch}""", file=sys.stderr)
 
     options = Options(debug=args.debug, quiet=args.quiet,
-        arch=arch, flags=rocmlir_gen_flags, concurrent_tests=args.jobs)
+        arch=arch, flags=rocmlir_gen_flags, concurrent_tests=args.jobs, numCU=getNumCU(arch))
     paths = perfRunner.create_paths(None, args.mlir_build_dir)
 
     config = args.config
